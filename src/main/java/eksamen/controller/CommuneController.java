130e258a979d0473fd44599aecb7c4e1d4e0b6b4
MatiasStorm <mat-435@hotmail.com>
2021-06-11 01:42:31 +0200

    remove unused imports

--- src/main/java/eksamen/controller/CommuneController.java
+++ src/main/java/eksamen/controller/CommuneController.java
@@ -1,8 +1,5 @@
 package eksamen.controller;
 
-import java.util.List;
-
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
